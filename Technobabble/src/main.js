1ab5cc29f6302ff3910263c6f6170777d5ec259f
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-01-18 16:02:57 -0500

    Added ajax folder and set up xhl

--- Technobabble/src/main.js
+++ Technobabble/src/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
 
 const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
@@ -11,10 +9,8 @@ init();
 // initialize function, applies onclick events to each button
 function init() {
     generatePhrase(1);
-    const singleButton = document.querySelector("#single-button");
-    const multiButton = document.querySelector("#multi-button");
-    singleButton.onclick = () => generatePhrase(1);
-    multiButton.onclick = () => generatePhrase(5);
+    document.querySelector("#single-button").onclick = () => generatePhrase(1);
+    document.querySelector("#multi-button").onclick = () => generatePhrase(5);
 }
 
 // returns a random element of the given array
@@ -24,10 +20,9 @@ function randomElement(array) {
 
 // generates a number of phrases line by line based on num
 function generatePhrase(num) {
-    let fullPhrase = "";
+    let fullPhrase = ``;
     for (let i = 0; i < num; ++i) {
-        const phrase = `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}`;
-        fullPhrase += `${phrase} <br>`;
+        fullPhrase += `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)} <br>`;
     }
     document.querySelector("#output").innerHTML = fullPhrase;
 }
