ba2b689c412f059afc37716b1588e9632c592627
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-01-20 16:30:15 -0500

    Added ajax-3 as xml loading example

--- XHR-helper-breakout-start/src/main.js
+++ XHR-helper-breakout-start/src/main.js
@@ -1,4 +1,7 @@
-import { loadTextXHR, displayTaffy, displayViking } from "./utils.js";
+import { loadTextXHR } from "./utils.js";
 
-document.querySelector("#button-1").onclick = () => loadTextXHR("data/taffy-facts.txt", displayTaffy);
-document.querySelector("#button-2").onclick = () => loadTextXHR("data/viking-facts.txt", displayViking);
+document.querySelector("#button-1").onclick = () => loadTextXHR("data/taffy-facts.txt", taffyCallback);
+document.querySelector("#button-2").onclick = () => loadTextXHR("data/viking-facts.txt", vikingCallback);
+
+const taffyCallback = e => document.querySelector("#output-1").innerHTML = e.target.responseText;
+const vikingCallback = e => document.querySelector("#output-2").innerHTML = e.target.responseText;
