872c9b0a05204fc2e4958a004bd21dc9da8ba27b
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-01-22 17:19:24 -0500

    Updated technobabble for csv

--- Technobabble/src/main.js
+++ Technobabble/src/main.js
@@ -1,14 +1,26 @@
-const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+let words1, words2, words3;
 
-const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+loadTextXHR();
 
-const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
-
-init();
+// loads text from csv file
+function loadTextXHR() {
+    const url = "data/babble-data.csv";
+    const xhr = new XMLHttpRequest();
+    xhr.onload = (e) => {
+        const text = e.target.responseText;
+        const lines = text.split("\n");
+        words1 = lines[0].split(",");
+        words2 = lines[1].split(",");
+        words3 = lines[2].split(",");
+    };
+    xhr.open("GET", url);
+    xhr.send();
+    init();
+}
 
 // initialize function, applies onclick events to each button
 function init() {
-    generatePhrase(1);
+    window.onload = () => generatePhrase(1);
     document.querySelector("#single-button").onclick = () => generatePhrase(1);
     document.querySelector("#multi-button").onclick = () => generatePhrase(5);
 }
@@ -22,7 +34,7 @@ function randomElement(array) {
 function generatePhrase(num) {
     let fullPhrase = ``;
     for (let i = 0; i < num; ++i) {
-        fullPhrase += `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)} <br>`;
+        fullPhrase += `${randomElement(words1)} ${randomElement(words2)} ${randomElement(words3)}<br>`;
     }
     document.querySelector("#output").innerHTML = fullPhrase;
 }
