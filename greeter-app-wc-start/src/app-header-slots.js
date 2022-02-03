b4bcc41759d271ddc43d4ba07e3f9cfd63120f9e
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-02-03 16:07:46 -0500

    Updated greeter for slots

---
+++ greeter-app-wc-start/src/app-header-slots.js
@@ -0,0 +1,27 @@
+const template = document.createElement("template");
+template.innerHTML = `
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
+<style>
+header {
+    user-select: none;
+}
+</style>
+<header class="hero is-small is-primary is-bold">
+    <div class="hero-body">
+      <div class="container">
+            <h1 class="title"><slot></slot></h1>
+            <h2 class="subtitle">Web Components Version</h2>
+        </div>
+    </div>
+</header>
+`;
+
+class AppHeaderSlots extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow({ mode: "open" });
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+}
+
+customElements.define('app-header-slots', AppHeaderSlots);
