a3bf5108b4ea1cefed0a953df5366163e49823c3
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-02-09 16:33:06 -0500

    Updated project documentation

--- project-1/src/app-footer.js
+++ project-1/src/app-footer.js
@@ -2,7 +2,9 @@ const template = document.createElement("template");
 template.innerHTML = `
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 <style>
+footer {
     user-select: none;
+}
 </style>
 <footer class="footer is-info">
     <div class="content has-text-centered">
--- project-1/src/app-header.js
+++ project-1/src/app-header.js
@@ -2,12 +2,14 @@ const template = document.createElement("template");
 template.innerHTML = `
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
 <style>
+header {
     user-select: none;
+} 
 </style>
 <header class="hero is-small is-info is-bold">
     <div class="hero-body">
         <div class="container has-text-centered">
-            <h1 class="title">Yu-Gi-Oh! Card Finder</h1>
+            <h1 class="title is-size-1">Yu-Gi-Oh! Card Finder</h1>
         </div>
     </div>
 </header>
