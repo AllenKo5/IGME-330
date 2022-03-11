587e0a9904ba2361a8e5c25ac7504cd259fd9f6b
AllenKo5 <69921088+AllenKo5@users.noreply.github.com>
2022-03-10 22:59:14 -0500

    Updated documentation and button to top of page

--- project-1/src/main.js
+++ project-1/src/main.js
@@ -4,5 +4,6 @@ import "./ygo-card.js";
 import "./app-navbar.js";
 
 document.querySelector("#top-button").onclick = () => {
+    document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
